6bb5de7e6a2e4a7e2cd197030450b9c84a3ef1ad
MegaSonicWarhead <51359439+MegaSonicWarhead@users.noreply.github.com>
2024-10-01 10:29:36 +0200

    Health bar works now and fixed pickups

---
+++ Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    // This method can be called for exiting the game (optional)
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
